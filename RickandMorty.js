9572361216233a23d1b7cf0b9b9288d38c7345ec
SmartGuy500 <rkkbeto@gmail.com>
2023-05-31 11:29:28 +0300

    First commit

---
+++ RickandMorty.js
@@ -0,0 +1,29 @@
+//Wubba lubba dub dub
+
+async function fetchData(){
+    const response = await fetch('https://rickandmortyapi.com/api/character')
+    const data = await response.json()
+    console.log(data)
+    data.results.forEach(function(character){
+        console.log(character.name)
+    }) 
+
+    data.results.forEach(function(character){
+        console.log(character)
+
+        const container = document.createElement('div')
+        container.classList.add('container')
+        container.innerHTML= `
+         <h1> ${character.name}</h1>
+         <h2> ${character.id}</h2>
+         <h2>Status: ${character.status}</h2> 
+         <h2>Species: ${character.species}</h2>
+         <a id=CharacterDetails href="${character.url}" target="_blank">Details</a>
+         <div class="image"><img src="${character.image}"/></div>
+        `
+
+        document.body.appendChild(container)
+    })
+
+}
+fetchData()
